b649a2b55ebee7fa2267d8fe10a1959e83a8aa94
Frans J Titulaer <fransjlt@gmail.com>
2022-12-09 01:32:04 +0100

    latest adjustments

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { TechStack } from "./TechStack";
+import styles from "../../styles/Home.module.scss";
 
 const About = () => {
   return (
-    <section className="fixed w-full h-full flex justify-end">
+    <section id={styles.about} className="fixed w-full h-full flex justify-end">
       <div className="w-6/12 h-full flex flex-col justify-between mr-16 p-12">
-        <div className="flex w-3/4 mt-10 justify-between items-start">
+        <div className="flex w-full mt-10 items-start">
           <Image src="/me.png" height="210em" width="210em" alt="" />
           <Info />
         </div>
@@ -19,20 +20,29 @@ export default About;
 
 const Info = () => {
   return (
-    <div>
+    <div className="ml-7">
       <h2 id="about" className="text-5xl font-thin">
         About
       </h2>
       <p className="py-2">
         Full-stack <b>Web developer</b>
       </p>
-      <p className="pt-5">
-        Passionate, <b>curious</b>, engaged
+      <p className="pt-2">
+        <span className={styles.fontChange}>Passion for </span>
+        <b>design</b>
       </p>
-      <p className="pt-3">
-        <b>Experienced</b> working in Edtech
+      <p className="pt-1">
+        <b>Experienced</b>
+        <span className={styles.fontChange}>
+          {" "}
+          within operations and support
+        </span>
       </p>
-      <p className="">operations, and support</p>
+      <p className={`pt-1 ${styles.fontChange}`}>curious and engaged</p>
+      <p className="pt-2">
+        <span className={styles.fontChange}>Knowledgable about </span>
+        <b>Edtech</b>
+      </p>
     </div>
   );
 };
