864202972e35a7b67b1c066c8698dd50307cd4fb
Frans Joakim Titulaer <fransjlt@gmail.com>
2025-06-09 22:20:09 +0200

    Added scroll down arrow animation

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -2,17 +2,25 @@ import { TechStack } from "./TechStack";
 import styles from "../../styles/About.module.scss";
 import { BoxAnimation, SmallBoxAnimation } from "./BoxAnimation";
 import Info from "./Info";
+import { FaArrowDownLong } from "react-icons/fa6";
 
 const About = () => {
   return (
-    <section id={styles.about}>
-      <div className={styles.container}>
-        <Info />
-        <BoxAnimation />
-        <SmallBoxAnimation />
-        <TechStack />
+    <>
+      <section id={styles.about}>
+        <div className={styles.container}>
+          <Info />
+          <BoxAnimation />
+          <SmallBoxAnimation />
+          <TechStack />
+        </div>
+      </section>
+      <div className="fixed w-screen h-[50px] bottom-0 flex flex-col justify-center items-center">
+        {/* @ts-ignore */}
+        <FaArrowDownLong color="gray" className={styles.jumpAnimation} />
+        <p className="text-xs text-gray-500">Scroll down</p>
       </div>
-    </section>
+    </>
   );
 };
 
